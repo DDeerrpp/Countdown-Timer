f52c4a876d16a0dfa447d36c4d710d777c28e8bb
DDeerrpp <raidon69869@icloud.com>
2023-05-28 14:23:48 -0700

    made timer live

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 var counters = document.querySelectorAll(".number");
-var time = [8, 23, 55, 41];
-
+var countDownDate = new Date("June 11, 2023 14:21:00").getTime();
+var currentTime = new Date().getTime();
+var distance = countDownDate - currentTime;
+var time = [
+            Math.floor(distance / (1000 * 60 * 60 * 24)),
+            Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+            Math.floor((distance % (1000 * 60)) / 1000)
+            ];
 counters.forEach((item, index) => {
-    item.innerText = (time[index].toString().length == 1)? "0"+time[index] : time[index]
+    item.innerText = time[index]
 })
+
 const countdown = setInterval(() => {
-    time[3] -= 1;
-    
-    if(time[0] == 0 && time[1] == 0 && time[2] == 0 && time[3] == 0) {
-        counters[3].innerText = '00';
-        clearInterval(countdown);
-    } else {
-        if(time[3] == 0 && time[2] != 0) {
-            time[3] = 60;
-            time[2] -= 1;
-        }
-        if(time[2] == 0 && time[1] != 0) {
-            time[2] = 59;
-            time[1] -= 1;
-        }
-        if(time[1] == 0 && time[0] != 0) {
-            time[1] = 11;
-            time[0] -= 1;
-        }
-    }
+    currentTime = new Date().getTime();
+    distance = countDownDate - currentTime;
+    time = [
+                Math.floor(distance / (1000 * 60 * 60 * 24)),
+                Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+                Math.floor((distance % (1000 * 60)) / 1000)
+               ];
     counters.forEach((item, index) => {
-        item.innerText = (time[index].toString().length == 1)? "0"+time[index] : time[index]
+        item.innerText = time[index]
     })
+    if (distance <= 0) {
+        clearInterval(countdown);
+    }
 }, 1000)
